ab64f8e8a88da7f765a36f0d967142c16f9a43bf
Kirill Bokhanov <kirill.bokhanov@ironsrc.com>
2016-07-01 12:30:44 +0300

    readme edited

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-# AtomRuby
+# ironSource.atom SDK for Ruby
+[![License][license-image]][license-url]
 
-Welcome to atom-ruby gem!
+Atom-Ruby is the official ironSource.atom SDK for the Ruby programming language.
+
+- [Signup](https://atom.ironsrc.com/#/signup)
+
+## Installation
+```bash
+$ gem install iron_source_atom-1.0.0.gem
+```
+### Example
+
+You can use our [example][example-url] for sending data to Atom.
+
+### License
+MIT
+
+[license-image]: https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square
+[license-url]: LICENSE.txt
 
--- lib/atom_ruby/http_client.rb
+++ lib/atom_ruby/http_client.rb
@@ -5,7 +5,10 @@ class HttpClient
 
   def post(url, data)
     uri = URI(url)
-    req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
+    initheader = {'Content-Type' =>'application/json',
+                  'x-ironsource-atom-sdk-type' => 'atom-ruby',
+                  'x-ironsource-atom-sdk-version' => IronSourceAtom::VERSION}
+    req = Net::HTTP::Post.new(uri.path, initheader)
     req.body = data
     response = Net::HTTP.new(uri.host, uri.port).start {|http| http.request(req) }
     puts "Request #{req.body}"
