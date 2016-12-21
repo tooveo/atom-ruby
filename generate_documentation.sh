ffd3081b6149ca4e2d1e519bd0bca40c5bd82448
g8y3e <valentine.pavchuk@ironsrc.com>
2016-12-21 12:57:28 +0200

    added generate_docs script

---
+++ generate_documentation.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+yardoc
+
+TARGET_BRANCH="gh-pages"
+mkdir $TARGET_BRANCH
+cd $TARGET_BRANCH
+
+git clone -b $TARGET_BRANCH --single-branch https://github.com/ironSource/atom-ruby.git
+
+cd atom-ruby
+cp -r ../../doc/* .
+
+# Remove old docs
+git rm .
+git commit -m "Clear GitHub Pages"
+
+# Now that we're all set up, we can push.
+git push origin $TARGET_BRANCH
+
+# Add new docs
+git add .
+git commit -m "Deploy to GitHub Pages"
+
+# Now that we're all set up, we can push.
+git push origin $TARGET_BRANCH
+
+cd ../../
+rm -r -f $TARGET_BRANCH
+rm -r -f doc
