87c9dabb2bc7a2ff7b556b14751270274dcbb4f8
g8y3e <valentine.pavchuk@ironsrc.com>
2017-01-13 13:45:04 +0200

    added few keys for compose and fixed integration test

--- test/integration_test_atom_sdk.rb
+++ test/integration_test_atom_sdk.rb
@@ -3,42 +3,77 @@ require 'iron_source_atom'
 
 puts "Integration test for ruby atom sdk!\n\n"
 
-class TestExample
-  	def self.do_test_job
-  		url = 'http://track.atom-data.io/'
-	    auth = ''
-	    stream = 'ibtest'
+class IntegrationTest
+  	def self.do_test
+  		# get arguments
+  		stream = ARGV[0]
+  		auth = ARGV[1]
+  		event_count = ARGV[2]
+  		bulk_size = ARGV[3]
+  		bulk_size_byte = ARGV[4]
+
+  		send_data_types = ARGV[5]
+  		data_key_increment = ARGV[6]
+
+  		puts "Args - stream: #{stream}; auth: #{auth}; even_count: #{event_count}; bulk_size: #{bulk_size}; \n bulk_size_byte: #{bulk_size_byte}; send_data_types: #{send_data_types}; data_key_increment: #{data_key_increment}"
+
+  		data_types = JSON.parse(send_data_types)
 
 	    atom_tracker = IronSourceAtom::Tracker.new
   		atom_tracker.auth = auth
+  		atom_tracker.bulk_size = bulk_size.to_i
+  		atom_tracker.bulk_size_byte = bulk_size_byte.to_i
   		atom_tracker.is_debug_mode = true
-
-  		event_count = ARGV[0]
 
   		puts "From test: #{event_count}"
 
-			atom_tracker.auth = "I40iwPPOsG3dfWX30labriCg9HqMfL"
+		stream = 'sdkdev_sdkdev.public.g8y3etest'
 
-			stream = 'sdkdev_sdkdev.public.g8y3etest'
+		prev_data = {}
+		prev_data[data_key_increment] = 0
 
-			for index in 0..event_count.to_i
-				#puts "Put event: #{index}"
-				data = {
-						id: index,
-						strings: "data index: #{index}"
-				}.to_json
-				atom_tracker.track(stream, data)
-				sleep(0.03)
+		for index in 0..event_count.to_i
+			#puts "Put event: #{index}"
+			data = {}
+
+			data_types.each do |key, value|
+				is_inc = data_key_increment == key
+				data_value = nil
+				if is_inc
+					prev_data[data_key_increment] =  IntegrationTest.generate_data(value, is_inc, prev_data[data_key_increment])
+					data_value = prev_data[data_key_increment]
+				else
+					data_value = IntegrationTest.generate_data(value)
+				end
+				data[key] = data_value
 			end
+		
+			atom_tracker.track(stream, data.to_json)
+			sleep(0.03)
+		end
 
-			atom_tracker.flush(lambda do |response|
-				puts "Test ran successfully!\n Response code: #{response.code}\n Response message #{response.message}"
-			end)
+		atom_tracker.flush(lambda do |response|
+			puts "Test ran successfully!\n Response code: #{response.code}\n Response message #{response.message}"
+		end)
 
-			sleep(10)
+		sleep(10)
   	end
 
-	do_test_job
+  	def self.generate_data(type, is_autoincrement = false, prev_value = nil)
+		case type
+		when "int"
+			if is_autoincrement
+				return prev_value + 1
+			else
+				return Random.rand(1000)
+			end
+		when "str"
+			return (0...(20 + Random.rand(100))).map { ('a'..'z').to_a[rand(26)] }.join
+		when "bool"
+			return Random.rand(1) == 1
+		end
+  	end
 
+	do_test
 end
 
--- test/run_integration.sh
+++ test/run_integration.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash -x
 echo "Event count: ${EVENT_COUNT}"
-ruby ./test/integration_test_atom_sdk.rb "${EVENT_COUNT}" 
+ruby ./test/integration_test_atom_sdk.rb "${STREAM}" "${AUTH}" ${EVENT_COUNT} ${BULK_SIZE} ${BULK_SIZE_BYTE} "${DATA_TYPES}" "${DATA_INCREMENT_KEY}"  
